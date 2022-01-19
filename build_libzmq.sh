ab5f28c49840b35e187035117d0a391109ea8c36
rsippl <260389+rsippl@users.noreply.github.com>
2022-01-19 18:47:42 +0100

    Fix libzmq build: simulator build was X86_64, now arm64

--- build_libzmq.sh
+++ build_libzmq.sh
@@ -3,16 +3,59 @@ set -euxo pipefail
 
 # libzmq:
 # create the static libraries
+
+IOS_SDKROOT=$(xcrun --sdk iphoneos --show-sdk-path)
+SIM_SDKROOT=$(xcrun --sdk iphonesimulator --show-sdk-path)
+IOS_VERSION_MIN=8.0
+OTHER_CPPFLAGS="-Os -fembed-bitcode"
+
 pushd libzmq
+ZMQ_DIR=$(pwd)
+IOS_BUILD_DIR="${ZMQ_DIR}/builds/ios/libzmq_build/arm64-ios"
+SIM_BUILD_DIR="${ZMQ_DIR}/builds/ios/libzmq_build/arm64-sim"
+
 if [[ -e Makefile ]]; then make distclean; fi
-sh builds/ios/build_ios.sh
+./autogen.sh
+
+mkdir -p ${IOS_BUILD_DIR}
+./configure \
+CC=clang CXX=clang++ \
+	CFLAGS="-arch arm64 -mios-version-min=${IOS_VERSION_MIN} -isysroot ${IOS_SDKROOT} ${OTHER_CPPFLAGS}" \
+	CPPFLAGS="-arch arm64 -mios-version-min=${IOS_VERSION_MIN} -isysroot ${IOS_SDKROOT} ${OTHER_CPPFLAGS}" \
+	CXXFLAGS="-arch arm64 -mios-version-min=${IOS_VERSION_MIN} -isysroot ${IOS_SDKROOT} ${OTHER_CPPFLAGS}" \
+	--prefix=${IOS_BUILD_DIR} \
+	--disable-shared \
+	--enable-static \
+	--host=arm-apple-darwin \
+	--disable-perf \
+	--disable-curve-keygen
+make -j8 
+make install
+make clean
+
+mkdir -p ${SIM_BUILD_DIR}
+./configure \
+	CC=clang CXX=clang++ \
+	CFLAGS="-arch arm64 -mios-simulator-version-min=${IOS_VERSION_MIN} -isysroot ${SIM_SDKROOT} ${OTHER_CPPFLAGS}" \
+	CPPFLAGS="-arch arm64 -mios-simulator-version-min=${IOS_VERSION_MIN} -isysroot ${SIM_SDKROOT} ${OTHER_CPPFLAGS}" \
+	CXXFLAGS="-arch arm64 -mios-simulator-version-min=${IOS_VERSION_MIN} -isysroot ${SIM_SDKROOT} ${OTHER_CPPFLAGS}" \
+	--prefix=${SIM_BUILD_DIR} \
+	--disable-shared \
+	--enable-static \
+	--host=arm-apple-darwin \
+	--disable-perf \
+	--disable-curve-keygen
+make -j8 
+make install
+make clean
+
 popd
 # then, merge them into XCframeworks:
 framework=libzmq
 rm -rf $framework.xcframework
 xcodebuild -create-xcframework \
-	-library $framework/builds/ios/libzmq_build/arm64/lib/libzmq.a -headers $framework/builds/ios/libzmq_build/arm64/include \
-	-library $framework/builds/ios/libzmq_build/x86_64/lib/libzmq.a -headers $framework/builds/ios/libzmq_build/x86_64/include \
+	-library ${IOS_BUILD_DIR}/lib/libzmq.a -headers ${IOS_BUILD_DIR}/include \
+	-library ${SIM_BUILD_DIR}/lib/libzmq.a -headers ${SIM_BUILD_DIR}/include \
 	-output $framework.xcframework
 
 
