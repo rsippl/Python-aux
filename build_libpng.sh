3093d397f3a0124eeeaeba8fcc5bc1c4548ade27
rsippl <260389+rsippl@users.noreply.github.com>
2022-01-19 19:51:29 +0100

    Fix libpng build: use arm64 instead of x86_64 for simulator

--- build_libpng.sh
+++ build_libpng.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euxo pipefail
 
 # Required with Xcode 12 beta:
 export M4=$(xcrun -f m4)
@@ -12,14 +13,17 @@ rm libpng-1.6.37.tar.gz
 export DYLD_ROOT_PATH\=$(xcrun --sdk iphonesimulator --show-sdk-path)
 
 # libpng
+
+binary=libpng
+
 pushd libpng-1.6.37
-make distclean
+if [[ -e Makefile ]]; then make distclean; fi
 ./configure CC=clang CXX=clang++ \
 	CFLAGS="-arch arm64 -miphoneos-version-min=11.0 -isysroot ${IOS_SDKROOT} -fembed-bitcode" \
 	CPPFLAGS="-arch arm64 -miphoneos-version-min=11.0 -isysroot ${IOS_SDKROOT} -fembed-bitcode" \
 	CXXFLAGS="-arch arm64 -miphoneos-version-min=11.0 -isysroot ${IOS_SDKROOT} -fembed-bitcode" \
-	--build=x86_64-apple-darwin --host=armv8-apple-darwin cross_compiling=yes
-make -j4 --quiet
+	--host=arm-apple-darwin
+make -j8
 # We're going to need them:
 mkdir -p ../libheif/lib_iphoneos
 mkdir -p ../libheif/include_iphoneos
@@ -27,7 +31,7 @@ cp .libs/libpng16.16.dylib ../libheif/lib_iphoneos/libpng.dylib
 cp png.h pnglibconf.h pngconf.h  ../libheif/include_iphoneos/
 # Library is now in: .libs/libpng16.16.dylib. Create framework:
 popd
-binary=libpng
+
 FRAMEWORK_DIR=build/Release-iphoneos/$binary.framework
 rm -rf ${FRAMEWORK_DIR}
 mkdir -p ${FRAMEWORK_DIR}
@@ -41,20 +45,23 @@ plutil -replace CFBundleExecutable -string $binary ${FRAMEWORK_DIR}/Info.plist
 plutil -replace CFBundleName -string $binary ${FRAMEWORK_DIR}/Info.plist
 plutil -replace CFBundleIdentifier -string Nicolas-Holzschuch.$binary  ${FRAMEWORK_DIR}/Info.plist
 install_name_tool -id @rpath/$binary.framework/$binary   ${FRAMEWORK_DIR}/$binary
+
+export DYLIB_INSTALL_NAME_BASE = @rpath
 pushd libpng-1.6.37
-make distclean
+if [[ -e Makefile ]]; then make distclean; fi
 ./configure CC=clang CXX=clang++ \
-	CFLAGS="-arch x86_64 -mios-simulator-version-min=11.0 -isysroot ${SIM_SDKROOT} -fembed-bitcode" \
-	CPPFLAGS="-arch x86_64 -mios-simulator-version-min=11.0 -isysroot ${SIM_SDKROOT} -fembed-bitcode" \
-	CXXFLAGS="-arch x86_64 -mios-simulator-version-min=11.0 -isysroot ${SIM_SDKROOT} -fembed-bitcode" \
-	--build=x86_64-apple-darwin --host=x86_64-apple-darwin cross_compiling=yes
-make -j4 --quiet
+	CFLAGS="-arch arm64 -mios-simulator-version-min=11.0 -isysroot ${SIM_SDKROOT} -fembed-bitcode" \
+	CPPFLAGS="-arch arm64 -mios-simulator-version-min=11.0 -isysroot ${SIM_SDKROOT} -fembed-bitcode" \
+	CXXFLAGS="-arch arm64 -mios-simulator-version-min=11.0 -isysroot ${SIM_SDKROOT} -fembed-bitcode" \
+	--host=arm-apple-darwin
+make -j8
 # We're going to need them:
 mkdir -p ../libheif/lib_iphonesimulator
 mkdir -p ../libheif/include_iphonesimulator
 cp .libs/libpng16.16.dylib ../libheif/lib_iphonesimulator/libpng.dylib 
 cp png.h pnglibconf.h pngconf.h  ../libheif/include_iphonesimulator/
 popd
+
 FRAMEWORK_DIR=build/Release-iphonesimulator/$binary.framework
 rm -rf ${FRAMEWORK_DIR}
 mkdir -p ${FRAMEWORK_DIR}
